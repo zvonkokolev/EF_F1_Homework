d5f6d5e4298cc42492c434a66cc48b6d7b5c3c6f
zvonkokolev <zvonko.kolev@liwest.at>
2020-03-07 13:16:37 +0100

    Projektdateien hinzufügen.

---
+++ Formula1.Core.Test/ImportControllerTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Formula1.Core.Test
+{
+	[TestClass()]
+	public class ImportControllerTests
+	{
+		/// <summary>
+		/// Als erste Übung die Rennen aus der XML-Datei parsen
+		/// </summary>
+		[TestMethod()]
+		public void T01_LoadRacesFromRacesXmlTest()
+		{
+			var races = ImportController.LoadRacesFromRacesXml().ToList();
+			Assert.AreEqual(21, races.Count());
+			Assert.AreEqual("Melbourne", races.First().City);
+			Assert.AreEqual(1, races.First().Number);
+			Assert.AreEqual("Abu Dhabi", races.Last().City);
+			Assert.AreEqual(21, races.Last().Number);
+		}
+
+		/// <summary>
+		/// Alle Results in Collections laden.
+		/// </summary>
+		[TestMethod()]
+		public void T02_LoadResultsFromResultsXmlTest()
+		{
+			var results = ImportController.LoadResultsFromXmlIntoCollections().ToList();
+			Assert.AreEqual(11, results.GroupBy(res => res.Team).Count());
+			Assert.AreEqual(24, results.GroupBy(res => res.Driver).Count());
+			Assert.AreEqual(462, results.Count);
+		}
+
+		[TestMethod()]
+		public void T03_LoadRacesFromRacesXmlTest()
+		{
+			var races = ImportController.LoadRacesFromRacesXml().ToList();
+			Assert.AreEqual("UAE", races.Last().Country);
+			Assert.AreEqual("Australia", races.First().Country);
+			Assert.AreEqual(20, races.Last().Number - 1);
+		}
+
+		/// <summary>
+		/// Results von Verstappen
+		/// </summary>
+		[TestMethod()]
+		public void T04_LoadVerstappenResults()
+		{
+			// Lade Verstappens Platzierungen in ein anonymes Objekt { City, Position }
+			// Sortiert nach der Rennnummer
+			var results = ImportController.LoadResultsFromXmlIntoCollections().ToArray();
+			var endResults = results
+				.Where(res => res.Driver.Name.Equals("Verstappen Max"))
+				.OrderBy(res => res.Race.Number)
+				.ToList();
+
+			Assert.AreEqual(21, endResults.Count);
+
+			Assert.AreEqual(10, endResults[0].Position);
+			Assert.AreEqual("Melbourne", endResults[0].Race.City);
+
+			Assert.AreEqual(6, endResults[14].Position);
+			Assert.AreEqual("Marina Bay", endResults[14].Race.City);
+		}
+	}
+}
---
+++ Formula1.Persistence/Migrations/20200307110515_InitialMigration.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Formula1.Persistence.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Races",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Number = table.Column<int>(nullable: true),
+                    Country = table.Column<string>(maxLength: 200, nullable: false),
+                    City = table.Column<string>(maxLength: 200, nullable: false),
+                    Date = table.Column<DateTime>(type: "date", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Races", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    Nationality = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Drivers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    Nationality = table.Column<string>(nullable: true),
+                    TeamId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Drivers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Drivers_Teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Results",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Position = table.Column<int>(nullable: false),
+                    Points = table.Column<int>(nullable: false),
+                    DriverId = table.Column<int>(nullable: false),
+                    TeamId = table.Column<int>(nullable: false),
+                    RaceId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Results", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Results_Drivers_DriverId",
+                        column: x => x.DriverId,
+                        principalTable: "Drivers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Results_Races_RaceId",
+                        column: x => x.RaceId,
+                        principalTable: "Races",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Results_Teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Drivers_TeamId",
+                table: "Drivers",
+                column: "TeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_DriverId",
+                table: "Results",
+                column: "DriverId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_RaceId",
+                table: "Results",
+                column: "RaceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Results_TeamId",
+                table: "Results",
+                column: "TeamId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Results");
+
+            migrationBuilder.DropTable(
+                name: "Drivers");
+
+            migrationBuilder.DropTable(
+                name: "Races");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+        }
+    }
+}
